0659484519ce3f28318e0c7b33472fb39db2c137
Lukas Löffler <lukasloeffler96@googlemail.com>
2020-11-08 19:55:40 +0100

    Added pulse animation to execution count if triggered

--- frontend-baklava/src/nodes/options/ExecutionCountOption.vue
+++ frontend-baklava/src/nodes/options/ExecutionCountOption.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <v-chip small label color="primary" class="mr-1" @click="resetCounter">Executed: {{executionCount}}</v-chip>
+        <v-chip small label color="primary" class="mr-1" v-bind:class="{ count: isActive }" @click="resetCounter">Executed: {{executionCount}}</v-chip>
     </div>
 </template>
 
@@ -10,6 +10,7 @@ export default {
     data: () => {
         return {
             executionCount: 0,
+            isActive: false,
         }
     },
     created() {
@@ -17,7 +18,11 @@ export default {
             try {
                 let data = JSON.parse(message.data);
                 if (data.node === this.node.id) {
+                    this.triggerOutInterfaces();
                     this.executionCount = data.callCount;
+                    this.isActive = true;
+                    let timeout = 1500; // timeout reset in case of 2 close consecutive calls
+                    setTimeout(() => this.isActive = false, timeout);
                 }
             } catch(err) {
                 console.log(err);
@@ -25,16 +30,37 @@ export default {
         }
     },
     methods: {
+        triggerOutInterfaces() {
+            //TODO: Implement rolling marbles to active connection.
+        },
         resetCounter() {
             console.log("ResettingCounter");
             let url = `http://localhost:3000/reset-exec-count/${this.node.id}`;
             this.axios.get(url).then(() => {
                 console.log("%cSuccessfully reset counter for", "color: green; font-weight: bold", this.node.name)
-            }); 
+            });
         }
     }
 }
 </script>
 
 <style>
+.count {
+    box-shadow: 0 0 0 0 rgba(255, 255, 255, 1);
+    animation: pulse 1.5s infinite !important;
+}
+
+@keyframes pulse {
+	0% {
+		box-shadow: 0 0 0 0 rgb(74, 123, 168);
+	}
+
+	70% {
+		box-shadow: 0 0 0 3px rgba(74, 123, 168);
+	}
+
+	100% {
+		box-shadow: 0 0 0 0 rgba(74, 123, 168);
+	}
+}
 </style>
