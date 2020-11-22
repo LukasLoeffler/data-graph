2e52d49184eb623fbe6e4822ca29f1bbe90ca68d
Lukas Löffler <lukasloeffler96@googlemail.com>
2020-11-22 14:48:59 +0100

    Removed object-utils

--- backend-node/src/nodes/data/json-path-node.ts
+++ backend-node/src/nodes/data/json-path-node.ts
@@ -1,7 +1,7 @@
 import { BaseNode } from "../base-node";
 import { NodeManager } from "../../nodes/node-manager";
 import { Message } from "../../message";
-const jsonUtils = require('../data/object-utils')
+var _ = require('lodash');
 
 const NODE_TYPE = "JSON_PATH"
 
@@ -16,7 +16,9 @@ export class ObjectPathNode extends BaseNode {
     }
 
     execute(msgIn: Message) {
-        let valueAtPath = jsonUtils.getValue(msgIn.payload, this.path);
+        //let valueAtPath = jsonUtils.getValue(msgIn.payload, this.path);
+
+        let valueAtPath = _.get(msgIn.payload, this.path);
         let msg = new Message(this.id, NODE_TYPE, valueAtPath);
         this.onSuccess(msg);
     }
