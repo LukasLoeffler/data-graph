196299551126460e8ebc67110f8b9ca7c60305a3
lukasloeffler <lukasloeffler96@googlemail.com>
2020-12-21 22:45:32 +0100

    Minor refactoring

--- backend-node/src/redis.ts
+++ backend-node/src/redis.ts
@@ -2,7 +2,7 @@ import chalk from "chalk";
 const redis = require("redis");
 const { promisify } = require("util");
 
-let url = "redis://192.168.178.82:6379/0"
+let url = "redis://193.196.39.113:6379/0"
 const client = redis.createClient(url);
 
 
