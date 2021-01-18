bad53ec3c2e75997b7748b825e1802d8fd6705b2
Lukas Löffler <lukasloeffler96@googlemail.com>
2021-01-18 09:47:23 +0100

    Light refactoring

--- frontend-baklava/src/nodes/time/IntervalNode.js
+++ frontend-baklava/src/nodes/time/IntervalNode.js
@@ -2,6 +2,10 @@ import { Node } from "@baklavajs/core";
 
 
 const intervals = [
+    {text: "every 10 sec", value: "*/10 * * * * *"},
+    {text: "every 20 sec", value: "*/20 * * * * *"},
+    {text: "every 30 sec", value: "*/30 * * * * *"},
+    {text: "every minute", value: "* * * * *"},
     {text: "every 5 min", value: "*/5 * * * *"},
     {text: "every 10 min", value: "*/10 * * * *"},
     {text: "every 15 min", value: "*/15 * * * *"},
