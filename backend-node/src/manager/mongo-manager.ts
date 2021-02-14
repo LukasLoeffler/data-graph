0518357c40961289262f9b7ba32181595da85b21
Lukas Löffler <lukasloeffler96@googlemail.com>
2021-02-14 10:31:04 +0100

    Added redis and mongo config for development

--- backend-node/src/manager/mongo-manager.ts
+++ backend-node/src/manager/mongo-manager.ts
@@ -1,9 +1,17 @@
+import chalk from "chalk";
+
 const MongoClient = require('mongodb').MongoClient;
 
 
-//let url = "mongodb://localhost:27017";
-//let url = "mongodb+srv://lukloe:removed@cluster0.oorug.mongodb.net?retryWrites=true&w=majority";
+
+const environment = process.env.NODE_ENV || 'DEV';
+
+console.log("Started application with ENV:", environment);
+
+
 let url = 'mongodb://mongo:27017';
+if (environment === "DEV") url = "mongodb://localhost:27017";
+
 
 let _db: any;
 
@@ -13,10 +21,17 @@ let requiredCollections = ["mqtt-servers", "workspaces", "node-configs", "node-t
 
 function connectToServer( callback: any ) {
     MongoClient.connect( url,  { useNewUrlParser: true, useUnifiedTopology: true }, function(err: any, client: any ) {
+
+        if (err) {
+            console.log(`Mongo-Server: (${url}): ${chalk.redBright("connecting failed")}`);
+            process.exit(1);
+        }
+
         _db  = client.db('mydb');
+        console.log(`Mongo-Server: (${url}): ${chalk.greenBright("connected")}`)
 
         requiredCollections.forEach((collection: any) => {
-            _db.createCollection("customers", function(err: any, res: any) {
+            _db.createCollection(collection, function(err: any, res: any) {
                 if (err) console.log(`Collection ${collection} already exists`);
                 else console.log(`Collection ${collection} successfully created`);
             });
