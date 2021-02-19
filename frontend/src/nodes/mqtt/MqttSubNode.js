3b466667be9b68b3e73e71af33c0c02964072ecd
lukasloeffler <lukasloeffler96@googlemail.com>
2021-02-19 22:58:17 +0100

    Added execCount

--- frontend/src/nodes/mqtt/MqttSubNode.js
+++ frontend/src/nodes/mqtt/MqttSubNode.js
@@ -21,7 +21,7 @@ export default class MqttSubNode extends Node {
             items: items
         });
         this.addOption("Topic", "InputOption", "topic")
-        this.addOption("ExecutionCount", "ExecutionCountOption")
+        this.addOption("ExecutionCount", "ExecutionCountOption");
         this.addOption("color", undefined, "#c5e364");
         this.addOption("running", undefined, true);
         this.addOutputInterface("onSuccess");
