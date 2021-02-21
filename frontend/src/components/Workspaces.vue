73f23451e58e7f1f898e9d47269f4f77020cc852
Lukas Löffler <lukasloeffler96@googlemail.com>
2021-02-21 16:47:10 +0100

    Added info text

--- frontend/src/components/Workspaces.vue
+++ frontend/src/components/Workspaces.vue
@@ -1,8 +1,11 @@
 <template>
     <v-card>
-        <v-btn class="d-flex ml-1 mt-1" color="green darken-2" small text @click="createWorkspace">
-            <v-icon color="green">mdi-shape-rectangle-plus</v-icon>
-        </v-btn>
+        <v-row>
+            <v-btn class="d-flex ml-2 mt-1" color="green darken-2" small text @click="createWorkspace">
+                <v-icon color="green">mdi-shape-rectangle-plus</v-icon>
+            </v-btn>
+            <v-subheader v-if="workspaces.length === 0" style="height: 35px">No workspaces present. Click on green symbol to add one.</v-subheader>
+        </v-row>
         <v-simple-table style="text-align:left">
             <template>
                 <thead>
