2643e00f5bff293537663119dffee20c17f7e8e7
Lukas Löffler <lukasloeffler96@googlemail.com>
2021-02-26 20:19:54 +0100

    Changed connection indicator

--- frontend/src/components/ConnectionIndicator.vue
+++ frontend/src/components/ConnectionIndicator.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-tooltip left v-if="status"  color="green darken-2">
+  <v-tooltip left v-if="status" color="green darken-2">
     <template v-slot:activator="{ on, attrs }">
-      <v-icon color="white" v-bind="attrs" v-on="on">mdi-lan-connect</v-icon>
+      <v-icon color="green lighten-3" v-bind="attrs" v-on="on">mdi-connection</v-icon>
     </template>
     <span>Connected with server</span>
   </v-tooltip>
   <v-tooltip left v-else color="red">
     <template v-slot:activator="{ on, attrs }" color="primary">
-      <v-icon  color="orange" v-bind="attrs" v-on="on">mdi-lan-disconnect</v-icon>
+      <v-icon  color="orange" v-bind="attrs" v-on="on">mdi-connection</v-icon>
     </template>
     <span>No server connection</span>
   </v-tooltip>
