2caea8c195210764334b951d77782aa9c226fa6a
Lukas Löffler <lukasloeffler96@googlemail.com>
2021-03-01 20:06:09 +0100

    Added basic wiki page

---
+++ frontend/src/views/Wiki.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card>
+    <v-card-title>Wiki: {{term}}</v-card-title>
+    <v-card-text v-if="term === 'Downstream'" style="text-align: left;">
+      # ┌────────────── second (optional)<br>
+      # │ ┌──────────── minute<br>
+      # │ │ ┌────────── hour<br>
+      # │ │ │ ┌──────── day of month<br>
+      # │ │ │ │ ┌────── month<br>
+      # │ │ │ │ │ ┌──── day of week<br>
+      # │ │ │ │ │ │<br>
+      # │ │ │ │ │ │<br>
+      # * &nbsp;* &nbsp;* &nbsp;* &nbsp;* &nbsp;*<br>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      term: null
+    }
+  },
+  created() {
+    this.term = this.$route.params.term.charAt(0).toUpperCase() + this.$route.params.term.slice(1);
+  }
+}
+</script>
+
+<style>
+
+</style>
