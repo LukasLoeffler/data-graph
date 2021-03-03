369651737b9ed7e2bfda3094fe3023b0b64aef07
Lukas Löffler <lukasloeffler96@googlemail.com>
2021-03-03 21:46:17 +0100

    Reworked custom context menu

--- frontend/src/components/CustomContextMenu.vue
+++ frontend/src/components/CustomContextMenu.vue
@@ -14,36 +14,41 @@
         include: include,
       }"
     >
-      <v-card width="700" class="included">
+      <v-card :width="width" class="included" transition="scale-transition" origin="center center">
         <v-card-title>
           <span class="headline">Add Node</span>
           <v-spacer></v-spacer>
           <v-text-field outlined label="Search Node Type" dense hide-details v-model="search" clearable></v-text-field>
+          <v-btn class="ml-2" dark @click="maxed = !maxed">
+            <v-icon v-if="maxed">mdi-window-maximize</v-icon>
+            <v-icon v-else>mdi-window-minimize</v-icon>
+          </v-btn>
         </v-card-title>
         <v-divider></v-divider>
         <v-card-text class="pb-0">
           <v-row v-if="nodeListFiltered && nodeListFiltered.length !== 0">
-            <v-col class="pb-0" style="max-height: 300px; overflow: scroll">
+            <v-col :cols="cols" class="pb-0" style="max-height: 300px; overflow: scroll">
               <v-chip-group active-class="primary--text" column v-model="selected" mandatory>
                 <v-chip
-                  v-for="node in nodeListFiltered"
-                  :key="node.node"
+                  v-for="(node, index) in nodeListFiltered"
+                  :key="index"
                 >
-                  {{ node.node }}
+                  {{ node.type }}
                 </v-chip>
               </v-chip-group></v-col>
             <v-col class="pb-0" style="text-align: left; max-height: 300px; overflow: scroll">
-              <h2 class="mb-3">{{nodeList[selected].node}}</h2>
-              <p v-html="nodeList[selected].description" style="font-size: 16px"></p>
-                  <v-chip 
-                    label small :key="tag" v-for="tag in nodeList[selected].tags" class="mr-1 mb-1"
-                    style="cursor: default" @click="search = tag"
-                  >      
-                    <v-icon left>
-                      mdi-tag-text-outline
-                    </v-icon>
-                    {{ tag }}
-                  </v-chip>
+              <h2 class="mb-3">{{nodeListFiltered[selected].type}}</h2>
+              <p v-html="nodeListFiltered[selected].description" style="font-size: 16px"></p>
+              <v-spacer></v-spacer>
+              <v-chip
+                label small :key="tag" v-for="tag in nodeListFiltered[selected].tags" class="mr-1 mb-1"
+                style="cursor: default" @click="search = tag" :class="{tagselected: tag === search}"
+              >      
+                <v-icon left>
+                  mdi-tag-text-outline
+                </v-icon>
+                {{ tag }}
+              </v-chip>
             </v-col>
           </v-row>
         </v-card-text>
@@ -72,7 +77,8 @@ export default {
       nodeList: null,
       valueCopy: false,
       selected: 0,
-      search: ""
+      search: "",
+      maxed: false
     }
   },
   methods: {
@@ -84,7 +90,7 @@ export default {
       } else {
         //console.log(`${submenu.label} -> ${submenu.value}`)
         this.nodeList.push({
-          node: submenu.label, 
+          type: submenu.label, 
           callstr: submenu.value, 
           description: getDescription(submenu.label),
           tags: getTags(submenu.label)
@@ -95,7 +101,7 @@ export default {
       return [document.querySelector('.included')]
     },
     addNode() {
-      this.onChildClick(`addNode:${this.nodeList[this.selected].node}`);
+      this.onChildClick(`addNode:${this.nodeListFiltered[this.selected].type}`);
     }
   },
   watch: {
@@ -108,14 +114,28 @@ export default {
       this.traverseSubmenues(this.items[0]);
     },
     search(newValue) {
+      this.selected = 0;
       if (newValue === null) this.search = "";
     }
   },
   computed: {
     nodeListFiltered() {
-      if (this.nodeList) return this.nodeList.filter(node => node.node.includes(this.search) || node.tags.some(tag  => tag.includes(this.search) ));
+      if (this.nodeList) return this.nodeList.filter(node => node.type.includes(this.search) || node.tags.some(tag  => tag.includes(this.search) ));
       else return null;
-    }
+    },
+    width() {
+      return (this.maxed ? 600 : 1200)
+    },
+    cols() {
+      return (this.maxed ? 6 : 4)
+    },
   }
 }
 </script>
+
+
+<style>
+.tagselected {
+  background-color: lightpink !important;
+}
+</style>
