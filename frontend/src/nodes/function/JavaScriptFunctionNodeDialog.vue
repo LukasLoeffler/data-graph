d53b4c1072d88c6efe3fa0c98b7336ac7c24ecab
Lukas Löffler <lukasloeffler96@googlemail.com>
2021-03-19 19:14:05 +0100

    Refactored input validation for JS-Node Dialog

--- frontend/src/nodes/function/JavaScriptFunctionNodeDialog.vue
+++ frontend/src/nodes/function/JavaScriptFunctionNodeDialog.vue
@@ -14,7 +14,7 @@
                 <v-col v-for="(intf, index) in outputInterfaces" :key="index" dense cols="12">
                   <v-text-field solo v-model="intf.name" prepend-icon="mdi-circle" hide-details>
                     <template v-slot:prepend>
-                        <v-icon color="green" v-if="valueCopy.code.includes(intf.name)">mdi-circle</v-icon>
+                        <v-icon color="green" v-if="portUsed(intf.name)">mdi-circle</v-icon>
                         <v-tooltip top v-else color="red">
                           <template v-slot:activator="{ on, attrs }">
                             <v-icon color="red" v-bind="attrs" v-on="on">mdi-circle</v-icon>
@@ -32,7 +32,7 @@
                 <v-col cols="12">
                   <v-text-field placeholder="New Interface" solo v-model="newName" hide-details>
                     <template v-slot:append>
-                      <v-btn text @click="addInterface" color="blue" :disabled="!newName">
+                      <v-btn text @click="addInterface" color="blue" :disabled="newNameInvalid">
                         CREATE
                       </v-btn>
                     </template>
@@ -110,7 +110,13 @@ export default {
     },
     save() {
       this.interfacesToRemove.forEach((intf) => {
-        if (!intf.isInput) this.node.removeInterface(intf.name);
+        if (!intf.isInput){
+          try {
+            this.node.removeInterface(intf.name);
+          } catch (error) {
+            console.log("Node not existing");
+          }
+        }
       });
 
       this.interfacesToAdd.forEach((intf) => {
@@ -144,9 +150,16 @@ export default {
       this.interfacesToAdd.push({name: this.newName, id: "NOT SAVED YET", isInput: false})
     },
     removeInterface(intf, index) {
-      this.outputInterfaces.splice(index, 1)
-      this.interfacesToRemove.push(intf)
+      this.outputInterfaces.splice(index, 1);
+      this.interfacesToAdd = this.interfacesToAdd.filter(intfToAdd => intfToAdd.id === intf.id);
+      this.interfacesToRemove.push(intf);
     },
+    portUsed(interfaceName) {
+      let variantA = `"${interfaceName}"`;  // Check for double quote intfName
+      let variantB = `'${interfaceName}'`;  // Check for single quote intfName
+
+      return this.valueCopy.code.includes(variantA) || this.valueCopy.code.includes(variantB);
+    }
   },
   watch: {
     "$store.getters.optionNode": {
@@ -157,6 +170,11 @@ export default {
           this.initInterfaceList();
         }
       }
+    }
+  },
+  computed: {
+    newNameInvalid() {
+      return this.outputInterfaces.some(intf => intf.name === this.newName) || !this.newName;
     }
   }
 }
