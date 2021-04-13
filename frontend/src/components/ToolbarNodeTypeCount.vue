16651cf0f8d3c35d26b782d6dbf0e06002f8d1f0
Lukas Löffler <lukasloeffler96@googlemail.com>
2021-04-13 10:14:52 +0200

    Added node count

---
+++ frontend/src/components/ToolbarNodeTypeCount.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="text-center">
+    <v-menu
+      open-on-hover
+      offset-y
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <h5 class="mr-3" v-bind="attrs" v-on="on">Node-Count: {{nodes.length}}</h5>
+      </template>
+
+      <v-list style="text-align: left">
+        <v-list-item
+          v-for="(value, name) in typeCount"
+          :key="name" dense
+        >
+          <v-list-item-title>{{ name }}:{{ value }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["nodes"],
+    data: () => ({}),
+    computed: {
+        typeCount() {
+          let typeCount = {};
+
+          this.nodes.forEach(node => {
+            if (typeCount[node.type] == null) typeCount[node.type] = 1;
+            else typeCount[node.type]++;
+          });
+          return typeCount;
+        }
+    }
+  }
+</script>
