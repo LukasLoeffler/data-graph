f9a9f670cab6f15b30eef001a6fece6df5b3bb36
Lukas Löffler <lukasloeffler96@googlemail.com>
2021-05-06 13:21:27 +0200

    Added yeelight node

---
+++ frontend/src/nodes/smarthome/YeelightDialog.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{nodeCopy.name}}</span>
+        </v-card-title>
+        <v-card-text class="pb-1">
+          <v-container>
+            <v-form v-model="valid">
+              <v-row>
+                <v-col cols="6">
+                  <v-text-field label="Name" :rules="[rules.required]" v-model="nodeCopy.name"></v-text-field>
+                </v-col>
+                <v-col cols="6">
+                  <v-text-field label="Bulp IP" :rules="[rules.required]" v-model="valueCopy.bulbIP"></v-text-field>
+                </v-col>
+                <v-col cols="6">
+                  <v-select label="Action" :rules="[rules.required]" v-model="valueCopy.action" :items="actions"></v-select>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-container>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-textarea
+          outlined label="Notes" class="px-5 py-2" rows="3" hide-details v-model="valueCopy.notes"
+        ></v-textarea>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">
+            Close
+          </v-btn>
+          <v-btn color="blue darken-1" :disabled="!valid" text @click="save">
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+
+<script>
+import EventBus from '@/event-bus';
+
+export default {
+  data: () => ({
+    dialog: false,
+    nodeCopy: null,
+    valueCopy: null,
+    rules: {
+      required: value => !!value || 'Required.',
+      positive: value => value > 0 || 'Positive number required.',
+    },
+    valid: false,
+    type: null,
+    actions: [
+        {text: "Turn On", value: "on"},
+        {text: "Turn Off", value: "off"},
+        {text: "Toggle", value: "toggle"},
+        {text: "Set Color", value: "color"},
+        {text: "Set Brightness", value: "brightness"},
+        {text: "Notify", value: "notify"}
+      ],
+  }),
+  props: ["option", "node", "value"],
+  created() {
+    this.init();
+    EventBus.$on("OPEN_SETTINGS", (nodeId) => {
+      if (nodeId === this.node.id) {
+        this.dialog = true;
+        this.init();
+      }
+    });
+  },
+  methods: {
+    save() {
+      this.node.setOptionValue("settings", this.valueCopy);
+      this.node.name = this.nodeCopy.name;
+      this.$store.commit("saveNodeConfig", this.node.id);
+      this.dialog = false;
+    },
+    init() {
+      this.nodeCopy = {...this.node};
+      this.valueCopy = JSON.parse(JSON.stringify(this.node.getOptionValue("settings")));
+    },
+  },
+}
+</script>
